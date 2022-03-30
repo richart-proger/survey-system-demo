22605b76c504dc9bde86c152cea17ec119241a89
Artur Galimov <satorica@mail.ru>
2022-03-30 20:39:24 +0300

    add spring context, hsqldb

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,13 @@
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', '{noop}password'),
+       ('Admin', 'admin@gmail.com', '{noop}admin');
+
+INSERT INTO user_roles (role, user_id)
+VALUES ('USER', 100000),
+       ('ADMIN', 100001);
